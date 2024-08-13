f43b9a76616fd87989952333ee177495c7e46de6
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-13 20:23:58 -0300

    Redirecting Routes According to Profile

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -3,14 +3,30 @@ import { BrowserRouter } from 'react-router-dom';
 import { useAuth } from "../hooks/auth";
 
 import { AdminRoutes } from './admin.routes';
+import { CustomerRoutes } from './customer.routes';
+import { SaleRoutes } from './sale.routes';
 import { AuthRoutes } from './auth.routes';
+import { USER_ROLE } from '../../utils/roles';
 
 export function Routes() {
   const { user } = useAuth();
 
+  function AcessRoute() {
+    switch(user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />
+      case USER_ROLE.SALE:
+        return <SaleRoutes />
+      default:
+        return <CustomerRoutes />
+    }
+  }
+
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AcessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
