ca302fad039fad1bb44af997bbff8c04a381c634
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-14 22:13:16 -0300

    Implement cookie back-end

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -27,9 +27,16 @@ class SessionsController {
       expiresIn
     });
 
+    response.cookie('token', token, {
+      httpOnly: true,
+      sameSite: 'Strict', /* Use 'none' if deploy */
+      secure: true,
+      maxAge: 15 * 60 * 1000
+    })
+
     delete user.password
 
-    response.status(201).json({ token, user });
+    response.status(201).json({ user });
   }
 }
 
